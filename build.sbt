5498004f68de1bbbd2cee98ed5078fc0c4499fb4
Stefan Schneider <schneist@gmail.com>
2021-05-06 09:09:33 +0200

    yolo

--- build.sbt
+++ build.sbt
@@ -6,7 +6,7 @@ enablePlugins(ScalablyTypedConverterPlugin)
 
 lazy val copyRes = TaskKey[Unit]("copyRes") //.dependsOn(fullOptJS)
 
-name := "ScaLambda-ImageAnon"
+name := "ScaLambda-Poll"
 
 version := "1.0.0"
 
@@ -15,8 +15,8 @@ scalaVersion := "2.13.5"
 
 libraryDependencies += "net.exoego" %%% "aws-lambda-scalajs-facade" % "0.11.0"
 libraryDependencies += "net.exoego" %%% "scala-js-nodejs-v12" % "0.13.0"
-libraryDependencies += "net.exoego" %%% "aws-sdk-scalajs-facade" % "0.32.0-v2.798.0"
-libraryDependencies +="com.lihaoyi" %%% "upickle" % "1.3.11"
+libraryDependencies += "net.exoego" %%% "aws-sdk-scalajs-facade" % "0.33.0-v2.892.0"
+libraryDependencies +="com.lihaoyi" %%% "upickle" % "1.3.12"
 libraryDependencies += "org.typelevel" %%% "cats-core" % "2.6.0"
 val cologVersion = "0.1.1"
 
@@ -25,26 +25,39 @@ libraryDependencies ++= Seq(
   "com.github.alonsodomin.colog" %% "colog-standalone" % cologVersion
 )
 
+libraryDependencies += "me.shadaj" %%% "slinky-core" % "0.6.7" // core React functionality, no React DOM
+libraryDependencies += "me.shadaj" %%% "slinky-web" % "0.6.7" // React DOM, HTML and SVG tags
+libraryDependencies += "me.shadaj" %%% "slinky-native" % "0.6.7" // React Native components
+libraryDependencies += "me.shadaj" %%% "slinky-hot" % "0.6.7" // Hot loading, requires react-proxy package
+libraryDependencies += "me.shadaj" %%% "slinky-scalajsreact-interop" % "0.6.7" // Interop with japgolly/scalajs-react
+libraryDependencies += "me.shadaj" %%% "slinky-react-router" % "0.6.7"
+
+scalacOptions += "-Ymacro-annotations"
+
 scalaJSLinkerConfig ~= (_.withModuleKind(ModuleKind.CommonJSModule))
 
 enablePlugins(ScalaJSBundlerPlugin)
 enablePlugins(ScalablyTypedConverterPlugin)
 
-
-
 copyRes := {
   import Path._
   val src = (Compile / target).value / "scala-2.13" / "scalajs-bundler" / "main"
-  val jsFiles: Seq[File] = (src ** "*.js").get()
-  val pairs = jsFiles pair rebase(src, baseDirectory.value / "handler")
+  val jsFiles: Seq[File] = (src ** "scalambda*.js").get()
+  val pairsJS = jsFiles pair rebase(src, baseDirectory.value / "handler")
+  val mapFiles: Seq[File] = (src ** "scalambda*.map").get()
+  val pairsMap = mapFiles pair rebase(src, baseDirectory.value / "handler")
   println("Copied:")
-  IO.copy(pairs, CopyOptions.apply(overwrite = true, preserveLastModified = true, preserveExecutable = false)).map(println)
-
+  IO.copy(pairsJS ++ pairsMap , CopyOptions.apply(overwrite = true, preserveLastModified = true, preserveExecutable = false)).map(println)
 }
 
+scalaJSUseMainModuleInitializer := true
+
+webpackBundlingMode := BundlingMode.LibraryAndApplication("appLibrary")
 
 Compile / npmDependencies ++= Seq(
   "@types/node" -> " 14.14.14",
-  "sharp" -> "0.28.0",
-  "@types/sharp" -> "0.28.0"
+  "react" -> "17.0.2",
+  "react-dom" -> "17.0.2",
+  "@types/react" -> "17.0.4",
+  "@types/react-dom" -> "17.0.3",
 )
--- src/main/scala/lambda/LambdaHandler.scala
+++ src/main/scala/lambda/LambdaHandler.scala
@@ -1,128 +1,133 @@
 package lambda
 
-import cats.effect._
-import cats.implicits._
-import facade.amazonaws.services.dynamodb._
-import facade.amazonaws.services.rekognition._
-import facade.amazonaws.services.s3._
 import net.exoego.facade.aws_lambda._
-import typings.sharp.mod._
+import org.scalajs.dom
+import org.scalajs.dom.{console, window}
+import slinky.core._
+import slinky.core.annotations._
+import slinky.web.html._
+import slinky.web.{ReactDOM, ReactDOMServer}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.scalajs.js
-import scala.scalajs.js.JSConverters.{JSRichFutureNonThenable, _}
+import scala.scalajs.js.JSConverters._
 import scala.scalajs.js.annotation._
-import scala.scalajs.js.{Dictionary, Promise}
+import scala.scalajs.js.{Dictionary, Promise, _}
 import scala.util.Try
-import cats.effect.IO
-import colog.{HasLogger, LogRecord, Logger}
-import cats.data.ReaderT
-import colog.Logging
-import cats.mtl.implicits._
+
+case class AnswerOption(label:String,value:Int)
+
+case class Question(text:String,answer:Option[Int],answerOptions : Seq[AnswerOption])
+
+object components {
+
+  val dummy = Seq(Question("Q1", Option.empty, Seq(AnswerOption("l1", 1), AnswerOption("l2", 2), AnswerOption("l3", 3))))
+
+  @react
+  object Page {
+
+    //ToDo:: remove hard code
+    val scriptloc = """https://d7yx4gnxnwmp1.cloudfront.net/scalambda-poll-fastopt-bundle.js"""
+
+    case class Props()
+
+    val component: FunctionalComponent[Props] = FunctionalComponent[Props] {
+      props =>
+
+        val Props() = props
+
+        html(
+          head(
+
+          ),
+          body(
+
+            div(id :="root")(
+              SurveyApp( dummy)
+            ),
+            script(src := scriptloc),
+          ),
 
 
-case class Env(logger: Logger[IO, LogRecord])
 
-object Env {
+        )
+    }
 
-  implicit val envHasLogger: HasLogger[IO, Env, LogRecord] = new HasLogger[IO, Env, LogRecord] {
-    override def getLogger(env: Env): Logger[IO, LogRecord] = env.logger
+  }
 
-    override def setLogger(env: Env)(newLogger: Logger[IO, LogRecord]): Env =
-      env.copy(logger = newLogger)
+
+  @react class SurveyApp extends Component {
+
+    case class Props(items: Seq[Question])
+
+    case class State(items: Seq[Question])
+
+    override def initialState = State(props.items)
+
+    def handleChange(e: SyntheticEvent[org.scalajs.dom.html.Input, org.scalajs.dom.Event]): Unit = {
+      val eventValue = e.target.value
+      eventValue
+      val mod : (State, Props) => State = (s,_) => s
+      setState(mod)
+    }
+
+    def handleSubmit(e: SyntheticEvent[org.scalajs.dom.html.Form, org.scalajs.dom.Event]): Unit = {
+      e.preventDefault()
+
+    }
+
+
+    override def render() = {
+      div(
+        h3("TODO"),
+        irn(state.items.head)
+      )
+    }
+
+    def irn(q:Question) ={
+      form(onSubmit := (handleSubmit(_)))(
+        div(h3(q.text)),
+        q.answerOptions.map(opt => button(name := opt.value.toString,opt.label))
+      )
+    }
+
+  }
+}
+
+object Main extends App {
+
+  console.log("Init main:")
+
+  if ( Try{! js.isUndefined(js.Dynamic.global.window)}.getOrElse(false)){
+    console.log("hydrate " )
+    hydrate()
+  }
+
+  @JSExportTopLevel("hydrate")
+  def hydrate(): Unit = {
+    val container = dom.document.getElementById("root")
+    ReactDOM.hydrate(
+      components.SurveyApp(components.dummy),
+      container
+    )
   }
 
 }
 
 object LambdaHandler {
-  type AppIO[A] = ReaderT[IO, Env, A]
-  val s3_SDK = new S3
-  val rekognition_SDK = new Rekognition
-  val dynamo_SDK = new DynamoDB
-  val logging = Logging.structured[AppIO, Env]
-
-  val log :AppIO[Unit] = logging.info("Hello")
-
-
-  def removeSubImages(sharp: Sharp,bounds:Seq[BoundingBox]) : Future[Sharp] ={
-    val blur = js.Dynamic.global.process.env.BLUR.asInstanceOf[js.UndefOr[String]].toOption.exists(_.equalsIgnoreCase("true"))
-    val radius = js.Dynamic.global.process.env.BLUR_RADIUS.asInstanceOf[js.UndefOr[Double]].toOption.getOrElse(25.0)
-    for {
-      m <- sharp.metadata().toFuture
-      h <- Future.fromTry(Try{m.height.get match {
-        case belowZero: Double  if belowZero <= 0  => 0
-        case good: Double => good
-      }})
-      w <-  Future.fromTry(Try{m.width.get match {
-        case belowZero: Double  if belowZero <= 0  => 0
-        case good: Double => good
-      }})
-      extracted <- bounds.map (b => {
-        val boxWidth = (b.Width.get * w) match {
-          case belowZero: Double  if belowZero <= 0  => 0
-          case good: Double => good
-        }
-        val boxHeight = (b.Height.get * h) match {
-          case belowZero: Double  if belowZero <= 0  => 0
-          case good: Double => good
-        }
-        val boxLeft = (b.Left.get * w) match {
-          case belowZero: Double  if belowZero <= 0  => 0
-          case good: Double => good
-        }
-        val boxTop = (b.Top.get * h) match {
-          case belowZero: Double  if belowZero <= 0  => 0
-          case good: Double => good
-        }
-        val filler: Future[typings.node.Buffer] = if (blur) {
-          sharp.toBuffer().toFuture.flatMap(b => typings.sharp.mod.apply(b)
-            .extract(Region.apply(boxLeft, boxTop, boxWidth, boxHeight))
-            .blur(radius).toBuffer().toFuture)
-        } else {
-          sharp.toBuffer().toFuture.flatMap(b => typings.sharp.mod.apply(b)
-            .extract(Region(boxLeft, boxTop, boxWidth, boxHeight))
-            .threshold(255).toBuffer().toFuture)
-        }
-        filler.map( x =>  OverlayOptions.apply().setInput(x).setLeft(boxLeft).setTop(boxTop))
-      }).toList.traverse(identity)
-      out <- Future.successful{
-        sharp.withMetadata().composite(extracted.toJSArray)
-      }
-    } yield out
-  }
-
-  def traverse(v: ujson.Value,Key:String): Iterable[String] = v match{
-    case a: ujson.Arr => a.arr.flatMap(traverse(_,Key))
-    case o: ujson.Obj if o.obj.contains(Key) => Seq(o.obj(Key).toString()+"#")
-    case o: ujson.Obj => o.obj.values.flatMap(traverse(_,Key))
-    case _ => Nil
-  }
 
   @JSExportTopLevel(name = "LambdaHandler")
-  def handle(event: S3Event, context: Context): Promise[Unit] = {
-    val e = event.Records.toList.head.s3
-    
-    val filename = js.URIUtils.decodeURIComponent(e.`object`.key.replace("+"," "))
+  def handle(event: APIGatewayProxyEvent, context: Context): Promise[APIGatewayProxyResult] = {
+    val headers :scala.scalajs.js.Dictionary[Boolean | Double | String] = Dictionary.apply(( "Content-Type", "text/html"))
     val o = for{
-      r_M <- rekognition_SDK.detectLabels(DetectLabelsRequest(Image(js.undefined,S3Object( e.bucket.name,filename)))).promise().toFuture
-      labels <- Future.successful(traverse(ujson.read(js.JSON.stringify(r_M.Labels)),"Name"))
-      confidences <- Future.successful(traverse(ujson.read(js.JSON.stringify(r_M.Labels)),"Confidence"))
-      _ <- dynamo_SDK.putItem(PutItemInput(Dictionary("Filename" -> AttributeValue(S=filename),"RekognitionLabels"-> AttributeValue.S(labels.mkString),"Confidences" -> AttributeValue.S(confidences.mkString),"raw" -> AttributeValue.S(js.JSON.stringify(r_M.Labels))),e.bucket.name)).promise().toFuture
-      s3 <- s3_SDK.getObject(GetObjectRequest( e.bucket.name,filename)).promise().toFuture
-      faceList <- Future.successful(r_M.Labels.get.filter(_.Name.equals("Person")).flatMap(_.Instances.getOrElse(Array.empty[Instance].toJSArray)))
-      sharp <-(s3.Body.get:Any) match {
-        case s:String =>  Future.successful(typings.sharp.mod.apply(s))
-        case a:scala.scalajs.js.Array[scala.Byte] =>  Future.successful(typings.sharp.mod.apply(a.map(_.toInt).join("")))
-        case _:Any => {
-          Future.successful(typings.sharp.mod.apply(""))
-        }
-      }
-      removed <- removeSubImages(sharp,faceList.map(_.BoundingBox.getOrElse(BoundingBox(0.0F,0.0F,0.0F,0.0F))).toSeq) //.filter(_.Height.getOrElse(0.0F)>=0))
-      buffer <- removed.toBuffer().toFuture
-      out <-s3_SDK.putObjectFuture(PutObjectRequest(e.bucket.name,filename.replace("input","output"),js.undefined,buffer.toString()))
+      out <- Future.successful(
+        ReactDOMServer.renderToString(
+          components.Page()
+        )
+      )
     } yield out
-    o.map[Unit](_ => ()).toJSPromise
+    o.map(s => APIGatewayProxyResult.apply(body = s,statusCode = 200, headers = headers, isBase64Encoded = false, multiValueHeaders = ())).toJSPromise
   }
-}
 
+}
