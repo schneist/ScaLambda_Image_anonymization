cd2bf758a5915be80a3f5bd8d2e3c7cbdf5acaff
Stefan Schneider <schneist@gmail.com>
2021-07-07 20:18:57 +0200

    shared front back

--- build.sbt
+++ build.sbt
@@ -1,5 +1,8 @@
 import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport.scalaJSLinkerConfig
 import sbt.Keys.{libraryDependencies, scalaVersion}
+import scalajsbundler.sbtplugin.ScalaJSBundlerPlugin.autoImport.additionalNpmConfig
+import scalajsbundler.util.JSON._
+
 
 lazy val commonSettings = Seq(
   version := "1.0.0",
@@ -15,16 +18,35 @@ lazy val commonSettings = Seq(
     "me.shadaj" %%% "slinky-scalajsreact-interop" % "0.6.7",
     "me.shadaj" %%% "slinky-react-router" % "0.6.7",
   )},
+  stFlavour := Flavour.Slinky,
   scalacOptions += "-Ymacro-annotations",
   scalaJSLinkerConfig ~= (_.withModuleKind(ModuleKind.CommonJSModule)),
-
+  webpackBundlingMode := BundlingMode.LibraryAndApplication("appLibrary"),
+  stFlavour := Flavour.Slinky,
+  Compile / additionalNpmConfig := Map(
+    "name"        -> str("Emil"),
+    "version"     -> str("1.0.0"),
+    "description" -> str("Awesome ScalaJS project..."),
+    "other"       -> obj(
+      "value0" -> bool(true),
+      "value1" -> obj(
+        "foo" -> str("bar")
+      )
+    )
+  )
 )
 
 lazy val shared = (project in file("shared"))
   .enablePlugins(ScalaJSPlugin)
+  .enablePlugins(ScalablyTypedConverterPlugin)
   .settings(commonSettings)
   .settings(
-
+    Compile / npmDependencies ++= Seq(
+      "react" -> "17.0.2",
+      "react-dom" -> "17.0.2",
+      "@types/react" -> "17.0.4",
+      "@types/react-dom" -> "17.0.3"
+    )
   )
 
 lazy val backend = (project in file("backend"))
@@ -57,8 +79,11 @@ lazy val frontend = (project in file("frontend"))
       "react-dom" -> "17.0.2",
       "@types/react" -> "17.0.4",
       "@types/react-dom" -> "17.0.3",
+      "xlsx" -> "0.17.0",
+      "js-yaml" ->  "4.1.0",
+      "@types/js-yaml" ->  "4.0.2"
     ),
-    webpackBundlingMode := BundlingMode.LibraryAndApplication("appLibrary"),
-    stFlavour := Flavour.Slinky
+    scalaJSUseMainModuleInitializer := true
+
   )
   .dependsOn(shared)
--- shared/src/main/scala/shared/Questionaire.scala
+++ shared/src/main/scala/shared/Questionaire.scala
@@ -1,108 +1,90 @@
 package shared
-import org.scalajs.dom.console
+import org.scalajs.dom
+import org.scalajs.dom.raw.{Blob, UIEvent}
+import org.scalajs.dom.{FileReader, console, window}
 import slinky.core._
 import slinky.core.annotations._
+import slinky.core.facade.{React, ReactElement}
 import slinky.web.html._
 
 import java.util.UUID
-
-case class AnswerOption(
-                         label:String,
-                         value:Int
-                       )
-
-case class Question(
-                     id:String = UUID.randomUUID().toString,
-                     text:String,
-                     answer:Option[Int],
-                     answerOptions : Seq[AnswerOption]
-                   )
+import scala.scalajs.js
+import scala.scalajs.js.Array.isArray
+import scala.scalajs.js.JSON
+import scala.scalajs.js.Object.{create, entries, keys}
+import scala.scalajs.js.typedarray.Uint8Array
+import slinky.core.facade.Hooks._
+import typings.std.global.TextEncoder
 
 
 object components {
 
-  val dummy = Seq(
-    Question(text = "Q1", answer = Option.empty, answerOptions = Seq(AnswerOption("l1", 1), AnswerOption("l2", 2), AnswerOption("l3", 3))),
-    Question(text = "Q2", answer = Option.empty, answerOptions = Seq(AnswerOption("l1", 1), AnswerOption("l2", 2), AnswerOption("l3", 3)))
-  )
 
   @react
   object Page {
 
     val scriptloc = """https://d7yx4gnxnwmp1.cloudfront.net/scalambda-poll-fastopt-bundle.js"""
 
-    case class Props(questions:scala.collection.immutable.Seq[Question])
+    case class Props(raw:String,reader: Uint8Array => String)
 
     val component: FunctionalComponent[Props] = FunctionalComponent[Props] {
       props =>
-
-
         html(
           head(
+            style("""
+                  #drop { border: 10px dashed #ccc; width: 300px; height: 300px; margin: 20px auto;}
+                  #drop.hover { border: 10px dashed #333; }
+                  """)
           ),
           body(
-
-            div(id :="root")(
-              SurveyApp( props.questions)
+            div(id := "root",
+              content.apply(props)
             ),
-            script(src := scriptloc),
-          ),
+            script(src := scriptloc)
+          )
         )
     }
+
+
+
+
+    val content:  FunctionalComponent[Props] = FunctionalComponent[Props] {
+      props => {
+        val (state, updateState) = useState(props)
+        div(id := "content",
+          div(id := "drop",
+            onMouseOver := { e =>
+              e.preventDefault()
+            },
+            onDragEnter := { e =>
+              e.preventDefault()
+            },
+            onDragOver := { e =>
+              e.preventDefault()
+            },
+            onDragEnd := { e =>
+              e.preventDefault()
+            },
+            onDrop := { e => {
+              e.preventDefault()
+              val file = e.asInstanceOf[js.Dynamic].dataTransfer.files.item(0)
+              val reader = new FileReader()
+              reader.onload = event => {
+                val data = new Uint8Array(event.target.asInstanceOf[js.Dynamic].result.asInstanceOf[js.Iterable[Short]])
+                updateState( Props(props.reader(data),props.reader))
+              }
+              reader.readAsArrayBuffer(file.asInstanceOf[Blob])
+            }
+            }),
+          div(id := "raw",
+            div(id := "rawtext", state.raw),
+            div(id := "outtext")
+          )
+        )
+      }
+    }
   }
 
-  @react class SurveyApp extends Component {
 
-    def answer(question: Question): Unit = {
-      setState(state => {
-        State(state.items.updated(question.id, question))
-      })
-    }
-
-    case class Props(items: scala.collection.immutable.Seq[Question])
-
-    case class State(items: scala.collection.immutable.Map[String,Question])
-
-    override def initialState = State(props.items.map(q => (q.id,q)).toMap)
-
-    override def render() = {
-      console.log("redner")
-      val filtered = state.items.filter(_._2.answer.isEmpty)
-      console.log(filtered)
-      div(
-        h3(filtered.head._2.id),
-        Answerer(filtered.head._2,answer(_))
-      )
-    }
-
-  }
-
-  @react class Answerer extends Component {
-    case class Props(item:Question,parent:Question => Unit)
-
-    case class State(item:Question,parent:Question => Unit)
-
-    override def initialState =State(props.item,props.parent)
-
-    override def componentWillReceiveProps(nextProps: Props): Unit = setState(State(nextProps.item,nextProps.parent))
-
-
-    def handleSubmit(e: SyntheticEvent[org.scalajs.dom.html.Form, org.scalajs.dom.Event]): Unit = {
-      e.preventDefault()
-      val l = state.item.copy(answer = Some(5))
-      console.log(state.parent)
-      state.parent(l)
-    }
-
-    override def render() = {
-
-      val q = state.item
-      form(onSubmit := (handleSubmit(_)))(
-        h3(q.text),
-        q.answerOptions.map(opt => button(name := opt.value.toString,opt.label))
-      )
-    }
-
-  }
 
 }
